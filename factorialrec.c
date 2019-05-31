5e78ebcc713209f684be3491fcda970494c7958d
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2019-05-31 16:23:04 +0530

    Create factorialrec.c

---
+++ factorialrec.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int fact(int);
+int main()
+{
+int n,f;
+printf("enter a number:");
+scanf("%d",&n);
+f=fact(n);
+printf("Factorial of %d is:%d",n,f);
+getch();
+return 0;
+}
+int fact(int n)
+{
+if(n==1)
+   return 1;
+else
+   return(n*fact(n-1));
+}
